6d846db34bf9d4446631875ef9184f40021468a8
yoojin1119 <yoojinpark@robotry.co.kr>
2022-08-27 19:14:29 +0900

    :memo: css 컨벤션 등록

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,37 @@
+const color = {
+    primary: '#91D7F2',
+    secondary: '#5196A6',
+    grey: "#CFCFCF",
+    backGround: 'EFEFEF',
+    black: "282828",
+    alert: "#F2594B"
+}
+
+
+const font = {
+    h1: 
+    `font-family: NEXON Gothic;
+    font-size: 64px;
+    line-height: 83.2px;`,
+    h2:
+    `font-family: NEXON Gothic;
+    font-size: 32px;
+    line-height: 41.6px;`,
+    h3:
+    `font-family: NEXON Gothic;
+    font-size: 28px;
+    line-height: 36.4px`,
+    h4:
+    `font-family: NEXON Gothic;
+    font-size: 20px;
+    line-height: 26px;`,
+    p:
+    `font-family: NEXON Gothic;
+    font-size: 16px;
+    line-height:20.8px`,
+}
+const theme = {
+    color,
+    font,
+}
+export default theme;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import theme from '../assets/styles/theme'
+import type { AppProps } from 'next/app'
+import {ThemeProvider} from '@emotion/react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+)}
+
+export default MyApp
