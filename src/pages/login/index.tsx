e7bb05874ec611d98cf638479d9c549938526d59
choisung2 <c0817d@gmail.com>
2022-09-30 09:37:07 +0900

    💄 로그인 페이지 UI 구현

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+// import Head from "next/head";
+import { LoginPage } from "../../components/login/Main";
+
+const Login: NextPage = () => {
+  return (
+    <section>
+      <h1 className="a11y-hidden">로그인 페이지</h1>
+      <LoginPage />
+    </section>
+  );
+};
+
+export default Login;
